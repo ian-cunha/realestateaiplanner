a9afa1991ae4aadaa5e1ee044679258a46a95a6c
Ian <iancunha20@gmail.com>
2024-08-15 17:31:15 -0300

    update redes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,14 @@ function Footer() {
 
     return (
         <div className="footer">
+            <div className='footer-div'>
+                <a className='icon-footer' href="https://www.instagram.com/realestateaiplanner/" target="_blank" rel="noopener noreferrer">
+                    <i className="bi bi-instagram fs-1"></i>
+                </a>
+                <a className='icon-footer' href="https://www.linkedin.com/company/real-estate-ai-planner" target="_blank" rel="noopener noreferrer">
+                    <i className="bi bi-linkedin fs-1"></i>
+                </a>
+            </div>
             <img src={Logo} alt="Real Estate Ai Planner" width="150" />
             <p className="footer-text text-center">@ 2024 Real Estate Ai Planner - Av. Barbosa Lima, 149 - Sala 416 - Recife, PE, 50030-330</p>
         </div>
